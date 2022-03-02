691a4ab2075f732f79440f40acada707918da5f9
rosaesguevara <rosaesguevara@gmail.com>
2022-03-02 17:41:10 -0600

    api municipio y tipo documento

---
+++ api/municipio/list.php
@@ -0,0 +1,25 @@
+<?php 
+	header("Access-Control-Allow-Origin: *");
+	header("Content-Type: application/json; charset=UTF-8");
+	header("Access-Control-Allow-Methods: POST, GET");
+	header("Access-Control-Max-Age: 3600");
+	header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+	include("../../config/class.connection.php");
+	include("../../class/class.municipio.php");
+
+	$municipio = new municipio();
+
+	$params = array();
+	$response = array();
+	
+	$response = $municipio->list();
+	if ($response["status"]=="success") {
+		if ($response["total"] == 0) {
+			$response = array("status"=>"error", "error" => "Municipio no disponible");
+		}
+	}
+
+	
+	echo json_encode($response);
+?>
---
+++ api/tipoDocumento/list.php
@@ -0,0 +1,25 @@
+<?php 
+	header("Access-Control-Allow-Origin: *");
+	header("Content-Type: application/json; charset=UTF-8");
+	header("Access-Control-Allow-Methods: POST, GET");
+	header("Access-Control-Max-Age: 3600");
+	header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+	include("../../config/class.connection.php");
+	include("../../class/class.tipoDocumento.php");
+
+	$tipoDocumento = new tipoDocumento();
+
+	$params = array();
+	$response = array();
+	
+	$response = $tipoDocumento->list();
+	if ($response["status"]=="success") {
+		if ($response["total"] == 0) {
+			$response = array("status"=>"error", "error" => "Tipo documento no disponible");
+		}
+	}
+
+	
+	echo json_encode($response);
+?>
